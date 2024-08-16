283fde7e944d4f6b4d3e03d56e6eca1e378bce7e
cakuakz <rafiirvansyah88@gmail.com>
2024-08-16 19:22:22 +0700

    feat: fixing caching data in get products

--- src/app/api/products/get-products/route.ts
+++ src/app/api/products/get-products/route.ts
@@ -1,14 +1,28 @@
-import { db } from "@vercel/postgres"
-import { NextResponse } from "next/server"
+import { db } from "@vercel/postgres";
+import { NextResponse } from "next/server";
 
 export const GET = async (req: Request) => {
     const client = await db.connect()
 
     try {
         const products = await client.sql`SELECT * FROM products ORDER BY created_at LIMIT 10 OFFSET 0;`
-        return NextResponse.json({ is_success: true, message: "Products fetched successfully", products: products.rows }, { status: 200 })
+        return NextResponse.json(
+            { is_success: true, message: "Products fetched successfully", products: products.rows },
+            {
+                status: 200,
+                headers: {
+                    "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+                    "Pragma": "no-cache",
+                    "Expires": "0",
+                    "Surrogate-Control": "no-store"
+                }
+            }
+        )
     } catch (error) {
-        console.error(error)
-        return NextResponse.json({ is_success: false, message: "Products fetch failed", error: error }, { status: 500 })
+        console.error(error);
+        return NextResponse.json(
+            { is_success: false, message: "Products fetch failed", error: error },
+            { status: 500 }
+        )
     }
 }
